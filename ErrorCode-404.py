791085602669119bd500d9fec63b146a7047b493
Tre Warner <treallenwarner@brandeis.edu>
2018-11-15 11:54:38 -0500

    Update ErrorCode-404.py

--- ErrorCode-404.py
+++ ErrorCode-404.py
@@ -1,5 +1,11 @@
 code = input("Input your code ")
-if (code[-1]==':'):
-    print ("Nothing is wrong with line one")
+print("Your program is:")
+print(code)
+
+if (code[0:2]=='def') or (code[0:1]=='if') or (code[0:3]=='else'):
+    if (code[-1]==':'):
+        print ("Nothing is wrong with line one")
+    else:
+        print ("you might be missing a colon in line one")
 else:
-    print ("you might be missing a colon in line one")
+    print("Nothing is wrong with line one")
