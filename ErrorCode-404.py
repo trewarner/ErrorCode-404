fd0df8294ed510c0ce6323be08e0a793eaaa06c8
Tre Warner <treallenwarner@brandeis.edu>
2018-11-19 11:45:04 -0500

    Update ErrorCode-404.py

--- ErrorCode-404.py
+++ ErrorCode-404.py
@@ -1,11 +1,42 @@
-code = input("Input your code ")
+code = str(input("Input your code: "))
 print("Your program is:")
 print(code)
 
-if (code[0:2]=='def') or (code[0:1]=='if') or (code[0:3]=='else'):
-    if (code[-1]==':'):
-        print ("Nothing is wrong with line one")
+def analyze(code):
+    if code.startswith("def"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
+    elif code.startswith("if"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
+    elif code.startswith("else"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
+    elif code.startswith("while"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
+    elif code.startswith("for"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
+    elif code.startswith("except"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
+    elif code.startswith("while"):
+        if not (code[-1]==':'):
+            print ("ERROR: Missing colon.")
+        else:
+            print ("No colon errors detected.")
     else:
-        print ("you might be missing a colon in line one")
-else:
-    print("Nothing is wrong with line one")
+        print ("No colon errors detected.")
