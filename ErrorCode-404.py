18670588b85ec097632762ada4378ac57f535657
Tre Warner <treallenwarner@brandeis.edu>
2018-11-28 11:43:10 -0500

    Update ErrorCode-404.py

--- ErrorCode-404.py
+++ ErrorCode-404.py
@@ -7,50 +7,32 @@ def analyze(code):
     if code.startswith("def"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
     elif code.startswith("if"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
     elif code.startswith("else"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
     elif code.startswith("while"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
     elif code.startswith("for"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
     elif code.startswith("except"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
     elif code.startswith("while"):
         if not (code[-1]==':'):
             print ("ERROR: Missing colon.")
-        else:
-            print ("No colon errors detected.")
-    else:
-        print ("No colon errors detected.")
 
-open = (code).count('(')
-closed = (code).count(')')
+    open = (code).count('(')
+    closed = (code).count(')')
 
-if open == closed:
-    print ("No parentheses/brackets errors deteced.")
-elif open > closed:
-    print ("There are more open parentheses/brackets than closed parentheses/brackets.")
-elif open < closed:
-    print ("There are more closed parentheses/brackets than open parentheses/brackets.")
+    if open > closed:
+        print ("There are more open parentheses/brackets than closed parentheses/brackets.")
+    elif open < closed:
+        print ("There are more closed parentheses/brackets than open parentheses/brackets.")
 
 filepath = input("Input program file name: ")
 
