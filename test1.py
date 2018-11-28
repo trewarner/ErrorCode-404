b67f0b68f7c3cf8da1649036b3c3ef9cb154ac46
Tre Warner <treallenwarner@brandeis.edu>
2018-11-28 11:45:59 -0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,8 @@
 def buggy()
   x = input("Are you happy? (yes or no)")
-  if (x=="no")
+  if (x=="no"))
     print("cheer up")
   elif (x=="yes")
-    print("that's nice")
+    print("that's nice"
   else
     print("I don't know what you mean...")
